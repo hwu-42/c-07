62fbe2d29cfdf0c0b3bbe16f8a2f249657eca30b
hwu <realwuhuidong@gmail.com>
2025-01-10 16:59:49 +0100

    clean

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,7 +2,33 @@
 #include <string>
 #include "whatever.hpp"
 
-int main( void ) {
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+int main(void)
+{
+	Awesome c(2), d(4);
+        swap(c, d);
+        std::cout << c << " " << d << std::endl;
+        std::cout << max(c, d) << std::endl;
+        std::cout << min(c, d) << std::endl;
+
+
+
     int a = 2;
     int b = 3;
     std::cout << "a = " << a << ", b = " << b << std::endl << "after swap" << std::endl;
@@ -11,13 +37,13 @@ int main( void ) {
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    std::cout << "c = " << c << ", d = " << d << std::endl << "after swap" << std::endl;
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    std::string e = "chaine1";
+    std::string f = "chaine2";
+    std::cout << "e = " << e << ", f = " << f << std::endl << "after swap" << std::endl;
+    ::swap(e, f);
+    std::cout << "e = " << e << ", f = " << f << std::endl;
+    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
 
 
 
